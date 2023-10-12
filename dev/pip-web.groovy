11df52fc93d79ea975b00f0d1ae8b27b85880ffc
longphanp <phanlonghd5200@gmail.com>
2023-10-12 14:39:15 +0700

    fix

--- dev/pip-web.groovy
+++ dev/pip-web.groovy
@@ -1,17 +1,17 @@
-pipelineJob('pip-web') {
+pipelineJob('pip-env') {
     definition {
         cpsScm {
             scm {
                 git {
                     remote {
                         name('origin')
-                        url('git@github.com:longphanp/pip-jenkins.git')
+                        url('git@github.com:longphanp/pip-env.git')
                         credentials('jenkins-git')
                     }
-                    branches('main')
+                    branches('dev')
                 }
             }
-            scriptPath('pip-web/dev/Jenkinsfile')
+            scriptPath('jenkins/Jenkinsfile')
         }
     }
 }
